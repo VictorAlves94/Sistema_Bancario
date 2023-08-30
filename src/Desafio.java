fd8aa7dd02fffdaecf0ba50415e1fffaa13ec05f
Victor Alves <alvesmlktop@gmail.com>
2023-08-30 10:29:57 -0300

    Criado o cliente

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nomeDoCliente = "Doutor Estranho";
+        String tipoConta = "Corrente";
+        double saldoAtual = 1599.99;
+
+        System.out.println("*******************************************");
+        System.out.println("\nNome do cliente : " + nomeDoCliente);
+        System.out.println("Tipo de conta : " + tipoConta);
+        System.out.println("Saldo atual da conta : " + saldoAtual);
+        System.out.println("\n*******************************************");
+    }
+}
